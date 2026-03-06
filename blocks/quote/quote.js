c3da5620925dd29dcb4470bf52e22c6c2f5289ad
LidermanGiraldoE <liderman.giraldo@omni.pro>
2026-03-05 23:15:57 -0500

    refactor: Eliminar la lógica de procesamiento de citas y autores en la función decorate

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,28 +1,7 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const rows = [...block.children];
-  const [quoteRow, authorRow] = rows;
-
-  // ✅ Navegar hasta donde realmente están los data-aue-*
-  const quoteInstrumented = quoteRow.querySelector('[data-aue-prop="quote"]');
-  const authorInstrumented = authorRow.querySelector('[data-aue-prop="author"]');
-
-  // --- QUOTE ---
-  const blockquote = document.createElement('blockquote');
-  blockquote.innerHTML = quoteInstrumented.innerHTML; // copia el <p> interno
-  moveInstrumentation(quoteInstrumented, blockquote);
-
-  // --- AUTHOR ---
-  const cite = document.createElement('cite');
-  cite.textContent = authorInstrumented?.textContent.trim() ?? '';
-  moveInstrumentation(authorInstrumented, cite);
-
-  // --- Estructura final ---
-  const figure = document.createElement('figure');
-  figure.appendChild(blockquote);
-  figure.appendChild(cite);
-
-  block.textContent = '';
-  block.appendChild(figure);
+  console.log('decorate quote', block);
+  const isAuthorEnv = window.xwalk?.isAuthorEnv;
+  console.log('isAuthorEnv', isAuthorEnv);
 }
