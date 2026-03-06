bd371d35f5a096b9de943efe4053a751f3f929c5
LidermanGiraldoE <liderman.giraldo@omni.pro>
2026-03-05 22:22:37 -0500

    refactor: Simplify decorate function by removing unused code and preserving logging

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,36 +1,3 @@
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  const [quoteWrapper, authorWrapper] = block.children;
-
-  const blockquote = document.createElement('blockquote');
-  const cite = document.createElement('cite');
-
-  // Move quote content preserving instrumentation
-  if (quoteWrapper) {
-    const quoteDiv = quoteWrapper.querySelector('div');
-    if (quoteDiv) {
-      // Move all children from the div to blockquote
-      while (quoteDiv.firstChild) {
-        blockquote.append(quoteDiv.firstChild);
-      }
-      moveInstrumentation(quoteDiv, blockquote);
-    }
-  }
-
-  // Move author content preserving instrumentation
-  if (authorWrapper) {
-    const authorDiv = authorWrapper.querySelector('div');
-    if (authorDiv) {
-      cite.textContent = authorDiv.textContent.trim();
-      moveInstrumentation(authorDiv, cite);
-    }
-  }
-
-  // Clear and rebuild the block
-  block.textContent = '';
-  block.append(blockquote);
-  if (cite.textContent) {
-    block.append(cite);
-  }
+  console.log('Decorating quote block', block);
 }
