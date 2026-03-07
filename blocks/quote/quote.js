f2ae3aa7916ecff2940a8075afcd7c608f84b75e
LidermanGiraldoE <liderman.giraldo@omni.pro>
2026-03-07 11:34:41 -0500

    refactor: Mejorar la función decorate al utilizar innerHTML y reemplazar el contenido del wrapper

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,11 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+  moveInstrumentation(quoteWrapper, blockquote);
+  blockquote.innerHTML = quoteWrapper.innerHTML;
+
+  quoteWrapper.replaceWith(blockquote);
 }
